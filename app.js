9e5a37a6b4a3fa3903c64596e88f15ad7d3f2366
Brandon Hinds <brandonhinds13@gmail.com>
2017-03-18 09:43:40 -0500

    Added Remove Option

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.controller("myCtrl", function($scope) {
     $scope.addItem = function(){
     	$scope.products.push($scope.addMe);
     }
+
+    $scope.removeItem = function (x) {
+        $scope.products.splice(x, 1);
+    }
 });
